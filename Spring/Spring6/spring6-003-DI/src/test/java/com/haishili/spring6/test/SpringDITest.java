b9e1ff76d5586c689e9f29046b05b688c8648274
Mesonsaber <1437317989@qq.com>
2024-10-07 15:58:04 +0800

    自动提交 2024-10-07 15:57:59

--- Spring/Spring6/spring6-003-DI/src/test/java/com/haishili/spring6/test/SpringDITest.java
+++ Spring/Spring6/spring6-003-DI/src/test/java/com/haishili/spring6/test/SpringDITest.java
@@ -1,6 +1,6 @@
 package com.haishili.spring6.test;
 
-import com.haishili.spring6.dao.UserDao;
+import com.haishili.spring6.service.CustomerService;
 import com.haishili.spring6.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -8,10 +8,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringDITest {
     @Test
-    public void test() {
+    public void testsetDI() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring6-002.xml");
         UserService userService = applicationContext.getBean("userServiceBean", UserService.class);
         userService.saveUser();
         userService.saveVip();
+    }
+    @Test
+    public void testconstructDI() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring6-003.xml");
+        CustomerService csBean = applicationContext.getBean("csBean", CustomerService.class);
+        csBean.save();
+
     }
 }
---
+++ hspjava/MAM_of_CO/src/main/java/com/override/Homework.java
@@ -0,0 +1,4 @@
+package com.override;
+
+public class Homework {
+}
