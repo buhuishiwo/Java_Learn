152a83bbe6eec760f465f46246cd8a4223a0eefa
Mesonsaber <1437317989@qq.com>
2024-10-18 16:08:26 +0800

    自动提交 2024-10-18 16:08:22

---
+++ hspjava/MAM_of_CO/src/main/java/com/smallchange/Client.java
@@ -0,0 +1,8 @@
+package com.smallchange;
+
+public class Client {
+    public static void main(String[] args) {
+        LQTSystem lqtSystem = new LQTSystem();
+        lqtSystem.showSystem();
+    }
+}
