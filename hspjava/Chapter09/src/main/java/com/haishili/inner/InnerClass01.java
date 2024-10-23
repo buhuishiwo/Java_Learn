5fe38c23315fb8f6c388b595f9d6296e5d264199
Mesonsaber <1437317989@qq.com>
2024-10-23 15:32:36 +0800

    自动提交 2024-10-23 15:32:29

---
+++ hspjava/Chapter09/src/main/java/com/haishili/inner/InnerClass01.java
@@ -0,0 +1,17 @@
+package com.haishili.inner;
+
+public class InnerClass01 {
+}
+
+//类五大成员：属性、方法、构造器、代码块、内部类
+class Outer {
+    //局部内部类
+    class Inner {
+
+    }
+
+}
+
+class Other{
+
+}
--- hspjava/Chapter09/src/main/java/com/haishili/interface_/InterfaceExercise.java
+++ hspjava/Chapter09/src/main/java/com/haishili/interface_/InterfaceExercise.java
@@ -1,6 +1,6 @@
 package com.haishili.interface_;
 
-public interface InterfaceExercise {
+public class InterfaceExercise {
     public static void main(String[] args) {
         B b = new B();
         System.out.println(b.a);
