656eb5f82dde7966fbc4beec7879c5d4710bcbe7
Mesonsaber <1437317989@qq.com>
2024-10-25 23:49:39 +0800

    自动提交 2024-10-25 23:49:35

--- hspjava/Chapter10/src/main/java/com/haishili/enum_/Enumeration03.java
+++ hspjava/Chapter10/src/main/java/com/haishili/enum_/Enumeration03.java
@@ -1,11 +1,19 @@
 package com.haishili.enum_;
 
 public class Enumeration03 {
+    public static void main(String[] args) {
+        System.out.println(Season2.SPRING);
+        System.out.println(Season2.AUTUMN);
+    }
 }
 
 //使用 enum 关键字
 enum Season2 {
-    SPRING("春天","温暖");
+    //使用逗号分割
+    SPRING("春天","温暖"),
+    SUMMER("夏天","炎热"),
+    AUTUMN("秋天","寒冷"),
+    WINTER("冬天","寒冷");
 
 
     private String name;
@@ -20,5 +28,13 @@ enum Season2 {
     }
     public String getDescription() {
         return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season2{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
     }
 }
