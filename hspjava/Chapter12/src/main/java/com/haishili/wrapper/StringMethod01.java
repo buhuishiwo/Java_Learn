193d42abbcd38babce43f71dda6b214a10d41515
Mesonsaber <1437317989@qq.com>
2024-11-09 23:03:54 +0800

    自动提交 2024-11-09 23:03:50

---
+++ hspjava/Chapter12/src/main/java/com/haishili/wrapper/StringMethod01.java
@@ -0,0 +1,17 @@
+package com.haishili.wrapper;
+
+public class StringMethod01 {
+    public static void main(String[] args) {
+        String s1 = "hale";
+        //获取字符在字符串中第一次出现的位置
+        System.out.println(s1.indexOf('e'));//3
+        //获取字符在字符串中最后一次出现的位置
+        System.out.println(s1.lastIndexOf('e'));//3
+        //返回指定字符串在原字符串中第一次开始出现的位置
+        System.out.println(s1.indexOf("al"));
+        //从索引 2 开始（包括 2）截取之后所有的内容
+        System.out.println(s1.substring(2));//le
+        //从索引 0 开始截取，截取到索引 4-0=0 的位置
+        System.out.println(s1.substring(0, 4));//hale
+    }
+}
