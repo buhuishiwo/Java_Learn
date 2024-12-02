8ada95e5a3761ca8d94ecc5e97a514542d82b301
Mesonsaber <1437317989@qq.com>
2024-12-02 20:03:53 +0800

    自动提交 2024-12-02 20:03:48

---
+++ JavaWeb/javaweb/servlet04/src/com/haishili/javaweb/httpservlet/PostServlet.java
@@ -0,0 +1,27 @@
+package com.haishili.javaweb.httpservlet;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PostServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html;charset=utf-8");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\">");
+        out.println("<title>Hello</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
